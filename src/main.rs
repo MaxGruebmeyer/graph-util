298bbb8f76467bc45764bc25241a32fe95876ec0
Max Gruebmeyer <max@gruebmeyer.de>
2024-12-25 19:27:19 +0100

    Moved generators to their own file

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,8 @@ use crate::datastructures::*;
 mod admat;
 use crate::admat::*;
 
-// TODO (GM): Test these methods!
-/// Generates a bidirectional path with size n
-fn generate_path(n: usize) -> DGraph {
-    let mut nodes: DGraph = Vec::new();
-    if n == 0 {
-        return nodes;
-    }
-
-    nodes.push(DNode::new(0));
-    for i in 1..n {
-        let cur = DNode::new(i.try_into().unwrap());
-        let last = nodes.last_mut().unwrap();
-
-        DEdge::new(&cur, last, DEFAULT_EDGE_WEIGHT);
-    }
-
-    nodes
-}
-
-/// Generates a bidirectional circle with size n
-fn generate_circle(n: usize) -> DGraph {
-    let nodes = generate_path(n);
-    if n < 2 {
-        return nodes;
-    }
-
-    let (first, rest) = nodes.split_first().unwrap();
-    let last = rest.last().unwrap();
-
-    DEdge::new(first, last, DEFAULT_EDGE_WEIGHT);
-
-    nodes
-}
-
-// TODO (GM): Other graph generation functions (complete, hypercube, ...)!
+mod generators;
+use crate::generators::*;
 
 fn print_admat(admat: &AdMat) {
     for row in admat {
