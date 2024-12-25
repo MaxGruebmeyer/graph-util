6a5c0fb1d546cd2150ae818ad559cd6507002923
Max Gruebmeyer <max@gruebmeyer.de>
2024-12-25 19:37:54 +0100

    Some cleanup

--- src/admat.rs
+++ src/admat.rs
@@ -100,4 +100,6 @@ pub fn print_admat(admat: &AdMat) {
 
         println!("");
     }
+
+    println!();
 }
--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,10 @@ mod generators;
 use crate::generators::*;
 
 fn main() {
-    println!("Hello world!");
-
-    // TODO (GM): Visualize these!
-
     let filename = "admat.txt";
     let admat = read_admat(&filename);
 
     print_admat(&admat);
-    println!();
-    print_admat(&graph_to_admat(admat_to_graph(&admat)));
-    println!();
-
-    let p5 = generate_path(5);
-    print_admat(&graph_to_admat(p5));
-    println!();
-
-    let c3 = generate_circle(3);
-    print_admat(&graph_to_admat(c3));
-    println!();
-
-    println!("Goodbye, cruel world!");
+    print_admat(&graph_to_admat(generate_path(4)));
+    print_admat(&graph_to_admat(generate_circle(7)));
 }
