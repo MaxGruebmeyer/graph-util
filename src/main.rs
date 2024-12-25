bbe9128d3b3c27b0167ef67d24dbcce7a49ea4b4
Max Gruebmeyer <max@gruebmeyer.de>
2024-12-25 19:25:40 +0100

    Extracted admat to its own file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
-extern crate regex;
-
 mod datastructures;
 use crate::datastructures::*;
 
-use std::{
-    collections::HashMap,
-    fs::File,
-    io::BufRead,
-    io::BufReader,
-};
-
-use regex::Regex;
+mod admat;
+use crate::admat::*;
 
 // TODO (GM): Test these methods!
 /// Generates a bidirectional path with size n
@@ -58,93 +50,8 @@ fn print_admat(admat: &AdMat) {
     }
 }
 
-fn read_admat(filename: &str) -> AdMat {
-    println!("Reading adjacency matrix from file '{}'", filename);
-
-    let mut mat: AdMat = Vec::new();
-    let num_regex = Regex::new(r"(\d+)").unwrap();
-
-    for line in BufReader::new(File::open(filename)
-        .expect("Could not open file!"))
-        .lines()
-        .map(|l| l.expect("Could not read line!")) {
-            let mut cur: Vec<Weight> = Vec::new();
-            for (_, [num]) in num_regex.captures_iter(&line)
-                 .map(|c| c.extract()) {
-                     cur.push(num.parse::<Weight>().unwrap());
-                 }
-
-            mat.push(cur);
-        }
-
-    mat
-}
-
-// TODO (GM): Make this generic?
-fn admat_to_graph(admat: &AdMat) -> DGraph {
-    let mut graph: DGraph = Vec::new();
-
-    for i in 0..admat.len() {
-        graph.push(Node::new(i as Value));
-    }
-
-    for i in 0..admat.len() {
-        for j in 0..admat.len() {
-            if admat[i][j] == 0 {
-                continue;
-            }
-
-            // TODO (GM): Does this work if a == b?
-            let a = &graph[i];
-            let b = &graph[j];
-
-            Edge::new(a, b, admat[i][j] as Weight);
-        }
-    }
-
-    graph
-}
-
-// TODO (GM): Documentation!
-fn graph_to_admat(graph: DGraph) -> AdMat {
-    let mut admat: AdMat = vec![vec![0; graph.len()]; graph.len()];
-
-    // HashMap to map values to positions.
-    // Assumes values are unique among nodes.
-    let mut pos_lookup: HashMap<Value, usize> = HashMap::new();
-
-    for i in 0..graph.len() {
-        match pos_lookup.insert(graph[i].borrow().val, i) {
-            None => {},
-            Some(x) => { panic!("Node values were not unique, {x} occurred multiple times!"); },
-        }
-    }
-
-    for node in graph {
-        for edge in &node.borrow().edges {
-            // TODO (GM): Does this actually work?
-
-            // Easier to calculate both positions here than to check which
-            //  one is which.
-            let i = *pos_lookup.get(&edge.a.borrow().val).unwrap();
-            let j = *pos_lookup.get(&edge.b.borrow().val).unwrap();
-
-            // Currently we only support undirected edges
-            admat[i][j] = edge.weight;
-            admat[j][i] = edge.weight;
-        }
-    }
-
-    admat
-}
-
 fn main() {
     println!("Hello world!");
-
-    // TODO (GM): Column-oriented or row-oriented?
-    // TODO (GM): Also allow 1d-adj_matrix?
-    // TODO (GM): Structs for nodes + methods to build adjacency-matrix from it!
-    let adj_matrix: Vec<Vec<Weight>> = Vec::new();
 
     // TODO (GM): Visualize these!
     // TODO (GM): Fix them + unit tests!
