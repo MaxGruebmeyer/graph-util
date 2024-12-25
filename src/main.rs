9ac63a46f51a5930f7341ac2442775e91591d8c2
Max Gruebmeyer <max@gruebmeyer.de>
2024-12-25 18:26:02 +0100

    Started with reworking nodes and edges struct (no idea what i am doing)

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,110 @@
 extern crate regex;
 
 use std::{
-    convert::TryInto,
+    cell::RefCell,
     fs::File,
     io::BufRead,
     io::BufReader,
-    rc::Rc,
+    rc::Rc
 };
 
 use regex::Regex;
 
 type Value = i32;
 type Weight = i8;
-type Graph<V, E> = Vec<GraphNode<V, E>>;
+type Graph<V, E> = Vec<Node<V, E>>;
 
 // TODO (GM): Because I'm lazy!
-type DGraphNode = GraphNode<Value, Weight>;
+type DGraphNode = Node<Value, Weight>;
 type DGraph = Graph<Value, Weight>;
-
 type AdMat = Vec<Vec<Weight>>;
 
 const DEFAULT_EDGE_WEIGHT: Weight = 1;
 
-// TODO (GM): Multiple structs for weighted, unweighted, directed, undirected graphs!
-// TODO (GM): Probably have to revisit this one!
-/// Struct representing a graph node with value of type V and
-///  edge weight of type E.
+// TODO (GM): Move all that shit into its own file!
 #[derive(Clone)]
-struct GraphNode<V, E> {
-    /// The value of the node
+struct Node<V, W> where V: Clone, W: Clone {
     val: V,
-
-    // TODO (GM): Do we also need incoming?
-    /// Outgoing connections
-    next: Vec<(E, Rc<GraphNode<V, E>>)>,
-
-    /// Bool signaling whether this node was visited before.
-    /// Useful for some algorithms.
-    /// It can be changed at any times, however no assumptions should be
-    /// made on this field before or after running an algorithm.
-    // TODO (GM): Make assumptions?
-    was_visited: bool,
+    // TODO (GM): Do some reading regarding Rc!
+    // TODO (GM): Think about thread-safety!
+    edges: Vec<Rc<Edge<V, W>>>,
 }
 
-impl<V,E> GraphNode<V, E> {
-    fn new(val: V) -> GraphNode<V, E> {
-        GraphNode {
+impl<V: std::clone::Clone, W: std::clone::Clone> Node<V, W> {
+    fn new(val: V) -> Node<V, W> {
+        Node {
             val,
-            was_visited: false,
-            next: Vec::new(),
+            edges: Vec::new(),
         }
+    }
+
+    fn get_deg(&self) -> usize {
+        self.edges.len()
+    }
+}
+
+// TODO (GM): Implement UnidirectionalEdge
+/// Represents a bidirectional edge connecting a to b.
+#[derive(Clone)]
+struct Edge<V, W> where V: Clone, W: Clone {
+    a: Rc<RefCell<Node<V, W>>>,
+    b: Rc<RefCell<Node<V, W>>>,
+    weight: W,
+}
+
+impl<V: std::clone::Clone, W: std::clone::Clone> Edge<V, W> {
+    // TODO (GM): Is there an option to provide a default value for weight?
+    fn new(a: &mut Node<V, W>, b: &mut Node<V, W>, weight: W) -> Edge<V, W> {
+        let edge = Edge {
+            // TODO (GM): Is cloning really the way?
+            a: Rc::new(RefCell::new(a.clone())),
+            b: Rc::new(RefCell::new(b.clone())),
+            weight,
+        };
+
+        a.edges.push(edge.clone().into());
+        b.edges.push(edge.clone().into());
+        edge
     }
 }
 
 // TODO (GM): For now: incoming = outgoing!
 
-/// Generates a bidirectional path with size n
-fn generate_path(n: usize) -> DGraph {
-    let mut nodes: DGraph = Vec::new();
-    if n == 0 {
-        return nodes;
-    }
-
-    nodes.push(DGraphNode::new(0));
-    for i in 1..n {
-        let mut cur = DGraphNode::new(i.try_into().unwrap());
-        let last: &mut DGraphNode = nodes.last_mut().unwrap();
-
-        // TODO (GM): There has to be a better way!
-        last.next.push((DEFAULT_EDGE_WEIGHT, cur.clone().into()));
-        cur.next.push((DEFAULT_EDGE_WEIGHT, (*last).clone().into()));
-    }
-
-    nodes
-}
-
-/// Generates a bidirectional circle with size n
-fn generate_circle(n: usize) -> DGraph {
-    let mut nodes = generate_path(n);
-    if n < 2 {
-        return nodes;
-    }
-
-    let (first, rest) = nodes.split_first_mut().unwrap();
-    let last = rest.last_mut().unwrap();
-
-    first.next.push((DEFAULT_EDGE_WEIGHT, (*last).clone().into()));
-    last.next.push((DEFAULT_EDGE_WEIGHT, (*first).clone().into()));
-    nodes
-}
+// TODO (GM): Fix!
+// /// Generates a bidirectional path with size n
+// fn generate_path(n: usize) -> DGraph {
+//     let mut nodes: DGraph = Vec::new();
+//     if n == 0 {
+//         return nodes;
+//     }
+// 
+//     nodes.push(DGraphNode::new(0));
+//     for i in 1..n {
+//         let mut cur = DGraphNode::new(i.try_into().unwrap());
+//         let last: &mut DGraphNode = nodes.last_mut().unwrap();
+// 
+//         // TODO (GM): There has to be a better way!
+//         last.next.push((DEFAULT_EDGE_WEIGHT, cur.clone().into()));
+//         cur.next.push((DEFAULT_EDGE_WEIGHT, (*last).clone().into()));
+//     }
+// 
+//     nodes
+// }
+// 
+// /// Generates a bidirectional circle with size n
+// fn generate_circle(n: usize) -> DGraph {
+//     let mut nodes = generate_path(n);
+//     if n < 2 {
+//         return nodes;
+//     }
+// 
+//     let (first, rest) = nodes.split_first_mut().unwrap();
+//     let last = rest.last_mut().unwrap();
+// 
+//     first.next.push((DEFAULT_EDGE_WEIGHT, (*last).clone().into()));
+//     last.next.push((DEFAULT_EDGE_WEIGHT, (*first).clone().into()));
+//     nodes
+// }
 
 // TODO (GM): Other graph generation functions (complete, hypercube, ...)!
 
