2543d42810186abc8c56eb5bd56941cf49f66c11
Max Gruebmeyer <max@gruebmeyer.de>
2024-12-26 18:58:54 +0100

    More visualization

--- src/visualization.rs
+++ src/visualization.rs
@@ -7,10 +7,10 @@ use std::{
 };
 
 /// Electrical constant used in the calculation of F_e
-const k_e: f64 = 1.0;
+const K_E: f64 = 1.0;
 
 /// Stability constant for calculation of F_s
-const k_s: f64 = 1.0;
+const K_S: f64 = 1.0;
 
 struct Position {
     x: f64,
@@ -24,14 +24,54 @@ struct EdgeId<V> where V: Clone, V: Eq, V: Hash {
     b: V,
 }
 
-fn get_2_norm(pos_a: &Position, pos_b: &Position) -> f64 {
-    ((pos_a.x - pos_b.x).powf(2.0)
-     + (pos_a.y - pos_b.y).powf(2.0)
-     + (pos_a.z - pos_b.z).powf(2.0)).sqrt()
+/// Get's the squared euclidean distance between a and b, e.g. (||b - a||_2)^2
+fn get_squared_2_norm(pos_a: &Position, pos_b: &Position) -> f64 {
+    (pos_b.x - pos_a.x).powf(2.0)
+     + (pos_b.y - pos_a.y).powf(2.0)
+     + (pos_b.z - pos_a.z).powf(2.0)
 }
 
-fn calculate_electrical_force(pos_a: &Position, pos_b: &Position) {
+/// Get's the euclidean distance between a and b, e.g. ||b - a||_2
+fn get_2_norm(pos_a: &Position, pos_b: &Position) -> f64 {
+    get_squared_2_norm(pos_a, pos_b).sqrt()
+}
 
+/// Subtracts the second vector from the first.
+fn sub(minuend: &Position, subtrahend: &Position) -> Position {
+    Position {
+        x: (minuend.x - subtrahend.x),
+        y: (minuend.y - subtrahend.y),
+        z: (minuend.z - subtrahend.z),
+    }
+}
+
+/// Multiplies a vector with a scalar
+fn mul(vec: &Position, mul: f64) -> Position {
+    Position {
+        x: vec.x * mul,
+        y: vec.y * mul,
+        z: vec.z * mul,
+    }
+}
+
+/// Get's the unit vector of a and b, e.g. (b - a)/||b - a||_2
+fn get_unit_vector(pos_a: &Position, pos_b: &Position) -> Position {
+    let inv_norm: f64 = 1.0 / get_2_norm(pos_a, pos_b);
+    mul(&sub(&pos_b, &pos_a), inv_norm)
+}
+
+/// Calculates the electrical force between two vertices a and b
+fn calculate_electrical_force(pos_a: &Position, pos_b: &Position,
+    charge_a: f64, charge_b: f64) -> Position {
+    let scalar: f64 = K_E * ((charge_a * charge_b) / get_squared_2_norm(pos_a, pos_b));
+    mul(&get_unit_vector(pos_a, pos_b), scalar)
+}
+
+/// Calculates the mechanical pullback force between two vertices a and b
+///  by using the old and new distance as well as the unit vector between them.
+fn calculate_mechanical_force(old_distance: f64, new_distance: f64,
+    unit_vector: &Position) -> Position {
+    mul(unit_vector, -K_S * (new_distance - old_distance))
 }
 
 fn charge_modifier(c: f64) -> f64 {
